e1d191f27db36c2e6b925927b641bc3a0baa9412
Rachel Katz <rachel.katz@flatironschool.com>
2024-02-22 12:38:35 -0500

    commit tree 7109228555139df0189798f990fc929d271ebfbe

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+let main = document.getElementById('main')
+// # is for ids, . is for classes
+main = document.querySelector('#main')
+main.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'YourName is the champion'
